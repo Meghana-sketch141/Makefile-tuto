8888a8e8630d76b9f42d577db3e4f8e2bf169f21
Meghana.S <97089631+Meghana-sketch141@users.noreply.github.com>
2024-09-04 23:59:52 +0530

    main.cpp

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+#include "function.h"
+
+using namespace std;
+
+// Main program
+int main()
+{
+    int num1 = 1;
+    int num2 = 2;
+    cout << multiply(num1, num2) << endl;
+    int num3 = 5;
+    cout << factorial(num3) << endl;
+    print();
+}
